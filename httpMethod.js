e771aab60c68b00ae2d67ab51fab0758cd82abec
Muhammad Naeem <muhammadnaeem39a@gmail.com>
2023-02-04 20:57:09 +0500

    http module

---
+++ httpMethod.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("Welcome to our home page");
+  res.end();
+  if (req.url === "/") {
+    res.end("Welcome to our home page.");
+  }
+  if (req.url === "/about") {
+    res.end("Welcome to our about page");
+  }
+  res.end(`
+    <h1>Oops</h1>
+    <p>This is not the page, you are finding</p>
+    <a href='/'>Home page</a>
+    `);
+});
+
+server.listen(5000);
